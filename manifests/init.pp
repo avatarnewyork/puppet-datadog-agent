05ce1e06a6c073d7217df242db44a8022a0468e7
patrick tully <patrick@avatarnewyork.com>
2013-03-19 15:38:47 -0400

    BAD: Editing template directly - but will be backwords compadible with cloudkick way requiring no changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,7 +27,6 @@ class datadog(
   $api_key = 'your key',
   $puppet_run_reports = false,
   $puppetmaster_user = 'puppet',
-  $tags = 'agent',
 ) inherits datadog::params {
 
   include datadog::params
