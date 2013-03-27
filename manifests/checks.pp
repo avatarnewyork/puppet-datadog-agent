ee47bebf3d7322e864c150504456639c3665d552
patrick tully <patrick@avatarnewyork.com>
2013-03-27 12:13:40 -0400

    Adding connection checking

--- manifests/checks.pp
+++ manifests/checks.pp
@@ -11,6 +11,8 @@
 # Sample Usage:
 #
 class datadog::checks {
+
+  # is_process_running Check
   file {"/etc/dd-agent/conf.d/is_process_running.yaml":
     ensure   => file,
     content  => template("datadog/is_process_running.yaml.erb"),
@@ -28,4 +30,23 @@ class datadog::checks {
     mode     => 0640,
     notify   => Service["datadog-agent"],
   }
+
+  # tcp_check Check
+  file {"/etc/dd-agent/conf.d/tcp_check.yaml":
+    ensure   => file,
+    content  => template("datadog/tcp_check.yaml.erb"),
+    owner    => "dd-agent",
+    group    => "root",
+    mode     => 0640,
+    notify   => Service["datadog-agent"],
+    require  => File["/etc/dd-agent"],
+  }
+  file {"/usr/share/datadog/agent/checks.d/tcp_check.py":
+    ensure   => file,
+    content  => template("datadog/tcp_check.py.erb"),
+    owner    => "dd-agent",
+    group    => "root",
+    mode     => 0640,
+    notify   => Service["datadog-agent"],
+  }  
 }
